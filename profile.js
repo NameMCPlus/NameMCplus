ec9438b260da240c0e63be9a6316683634f670a1
M6 <56744133+M6yo@users.noreply.github.com>
2021-06-15 13:28:31 -0400

    Update profile.js

--- profile.js
+++ profile.js
@@ -11,21 +11,34 @@ function setStatus(username) {
 .then(data => {
   var type = data.status;
   var uuid = data.uuid
-  if (type == "msa") {
-    accountType = "Microsoft";
-  } else if (type == "migrated_msa") {
-    accountType = 'Microsoft';
-  } else if (type == "new_msa") {
-    accountType = 'Microsoft';
-  } else if (type == "mojang") {
-    accountType = "Mojang";
-  } else if (type == "normal") {
-    accountType = "Normal (Mojang OR Microsoft)";
-  } else if (type == "legacy") {
-    accountType = "Legacy";
-  } else if (uuid.toLowerCase() == "cc7ba16bb2c540fca18eb87b6949270e") { // homara- breaks the API lol
+
+
+  switch(type) {
+
+    case "msa":
+      accountType = "Microsoft";
+        break;
+    case "migrated_msa":
+      accountType = "Microsoft";
+        break;
+    case "new_msa":
+      accountType = 'Microsoft';
+        break;
+    case "mojang":
+      accountType = "Mojang";
+        break;
+    case "normal":
+      accountType = "Normal (Mojang OR Microsoft)";
+        break;
+    case "legacy":
+      accountType = "Legacy";
+        break;
+  }
+
+  if (uuid.toLowerCase() == "cc7ba16bb2c540fca18eb87b6949270e") { // homara- breaks the API lol)
     accountType = "Legacy";
   }
+  
   template.innerHTML = `<div class="row no-gutters"><div class="col order-lg-1 col-lg-4"><strong>Account Type</strong></div><div class="col-auto order-lg-3 col-lg-auto text-nowrap text-right">${accountType}</div></div>`;
   var viewsElement = document.querySelectorAll(".row, .no-gutters")[5];
   var accountTypeElement = template.content.children[0];
