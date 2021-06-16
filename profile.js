5ff6a99836a7c30aa28280bca7eab240911f818d
Faav <52789876+withdrew@users.noreply.github.com>
2021-06-16 15:29:13 -0400

    Update profile.js

--- profile.js
+++ profile.js
@@ -47,7 +47,7 @@ function setStatus(username) {
   }
 
   template.innerHTML = `<div class="row no-gutters"><div class="col order-lg-1 col-lg-4"><strong>Account Type</strong></div><div class="col-auto order-lg-3 col-lg-auto text-nowrap text-right">${accountType}</div></div>`;
-  var viewsElement = document.querySelectorAll(".row, .no-gutters")[4];
+  var viewsElement = document.querySelectorAll(".col-lg-4")[3].parentElement;
   var accountTypeElement = template.content.children[0];
   viewsElement.parentNode.insertBefore(accountTypeElement, viewsElement.nextSibling);
 });
