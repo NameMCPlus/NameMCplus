d395f4d62d5b433eee3f057ee0e0eccc75d15e71
Faav <52789876+withdrew@users.noreply.github.com>
2021-06-22 12:39:55 -0400

    Update toggleSettings.js

--- toggleSettings.js
+++ toggleSettings.js
@@ -1,36 +1,19 @@
-document.querySelector("#hideblcbutton").addEventListener("click", toggleBLC());
+$("#hideblcbutton").on('change', function() {toggleBLC()});
 
   function toggleBLC() {
-
-    var HideBLC;
-
-    document.querySelector("#BLCStatus").innerHTML = HideBLC;
-
-    switch (HideBLC) {
-
-        case false:
-            HideBLC = true;
-                break;
-        case true:
-            HideBLC = false;
-                break;
-        case null:
-        case undefined:
-            HideBLC = true;
-                break;
-    }
+    var HideBLC = hideblcbutton.checked;
     
-    chrome.storage.local.set({HideBLC: HideBLC}, function() {
+    chrome.storage.local.set({"HideBLC": HideBLC}, function() {
         console.log("Saved");
     });
+
     console.log(HideBLC);
-    
-    document.querySelector("#BLCStatus").innerHTML = HideBLC;
   }
 
   function restore_options() {
     chrome.storage.local.get(['HideBLC'], function(result) {
-      console.log("Options loaded");
+      hideblcbutton.checked = result.HideBLC;
+      console.log("Options loaded: " + result.HideBLC);
     });
   }
 
