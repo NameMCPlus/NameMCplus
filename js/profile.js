84ec8cf6ab207dc0180c037201d1c3b018396bb7
Faav <52789876+withdrew@users.noreply.github.com>
2021-06-23 12:13:39 -0400

    Update profile.js

--- js/profile.js
+++ js/profile.js
@@ -1,79 +1,39 @@
 //by M6 (M6yo) & Faav (withdrew)
 
-var username = document.querySelector("div.col.order-lg-1.col-lg-4.text-nowrap").firstElementChild.innerHTML;
-var template = document.createElement("template");
+var username = document.querySelector(".my-1, .px-3").firstElementChild.innerHTML;
+var namemcStatus = document.querySelector(".col-sm-6.my-1").lastElementChild;
 
-chrome.storage.local.get(['HideBLC'], function (result) {
-  if (result.HideBLC == true) {
-    document.querySelector(".badlion-card").remove();
-  }
-});
-
-$('body').tooltip({
-  selector: '[data-toggle=tooltip]'
-});
-
-//Formats Creation Dates
-function formatCreation(date) {
-  var d = date.split("-");
-  var date = `${d[1]}/${d[2]}/${d[0]}`;
-  return date;
+function removeFinePrint() {
+  // removes fine print under name availability
+  var a = document.querySelector("body > main > div.mb-3 > small");
+  a.remove("d-block text-right");
 }
 
-setStatus(username);
+betterNameMC(username);
 
-function setStatus(username) {
-  fetch(`https://api.ashcon.app/mojang/v2/user/${username}`)
-    .then(response => response.json())
-    .then(ashcon => {
-      fetch(`https://api.gapple.pw/status/${username}`)
-        .then(response => response.json())
-        .then(data => {
-          var type = data.status;
-          var uuid = data.uuid
-          var createdAt = ashcon.created_at;
-          var accountType;
-          var tooltip;
-
-          switch (type) {
-            case "msa":
-              accountType = "Microsoft";
-              tooltip = "Microsoft Account";
-              break;
-            case "migrated_msa":
-              accountType = "Microsoft";
-              tooltip = "Migrated from Mojang";
-              break;
-            case "new_msa":
-              accountType = 'Microsoft';
-              tooltip = "Newly Created";
-              break;
-            case "mojang":
-              accountType = "Mojang";
-              tooltip = "Mojang Account";
-              break;
-            case "normal":
-              accountType = "Normal (Mojang OR Microsoft)";
-              tooltip = "Mojang or Microsoft Account";
-              break;
-            case "legacy":
-              accountType = "Legacy";
-              tooltip = "Unmigrated (2009-Late 2012)";
-              break;
+function betterNameMC(username) {
+  fetch(`https://api.gapple.pw/blocked/${username}`)
+    .then(response => {
+      if (response.ok) return response.json();
+      return response.json().then(response => response)
+    })
+    .then(data => {
+        chrome.storage.local.get(function (result) {
+            var status = data.status;
+            var blockedNameBtn = result.blockedNames;
+            if (blockedNameBtn == true) {
+              if (status == "blocked" || status == "blocked_or_dropping" && namemcStatus.innerHTML == "Available*") {
+                removeFinePrint();
+                namemcStatus.innerHTML = "Blocked";
+                var element = document.getElementById('status-bar');
+                element.classList.remove("bg-success");
+                element.style.backgroundColor = "grey";
+              } else if (namemcStatus.innerHTML == "Available*") {
+                removeFinePrint();
+                namemcStatus.innerHTML = "Available";
+              }
+            }
           }
-
-          if (uuid == "cc7ba16bb2c540fca18eb87b6949270e") { // homara- breaks the API lol)
-            accountType = "Legacy";
-            tooltip = "Unmigrated (2009-Late 2012)";
-          }
-          if (createdAt) {
-            template.innerHTML = `<div class="row no-gutters"><div class="col order-lg-1 col-lg-4"><strong>Account Type</strong></div><div class="col-auto order-lg-3 col-lg-auto text-nowrap text-right" data-toggle="tooltip" title="${tooltip}">${accountType}</div></div><div class="row no-gutters"><div class="col order-lg-1 col-lg-4"><strong>Created At</strong></div><div class="col-auto order-lg-3 col-lg-auto text-nowrap text-right" data-toggle="tooltip" data-html="true" title="<b>Creation dates are inaccurate for a lot of accounts due to a breaking change on Mojang's end. We are currently fetching dates from Ashcon's API. Please yell at Mojang (WEB-3367) in order for accurate creation dates to return.</b>">${formatCreation(createdAt)}</div></div>`;
-          } else {
-            template.innerHTML = `<div class="row no-gutters"><div class="col order-lg-1 col-lg-4"><strong>Account Type</strong></div><div class="col-auto order-lg-3 col-lg-auto text-nowrap text-right" data-toggle="tooltip" title="${tooltip}">${accountType}</div></div>`;
-          }
-          var viewsElement = document.querySelectorAll(".col-lg-4")[3].parentElement;
-          var accountTypeElement = template.content;
-          viewsElement.parentNode.insertBefore(accountTypeElement, viewsElement.nextSibling);
         });
     });
 }
