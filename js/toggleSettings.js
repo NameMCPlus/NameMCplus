117a54d607655fe39fba9336ffac1cad94146595
Faav <52789876+withdrew@users.noreply.github.com>
2021-06-23 12:13:06 -0400

    Update toggleSettings.js

--- js/toggleSettings.js
+++ js/toggleSettings.js
@@ -1,20 +1,75 @@
-$("#hideblcbutton").on('change', function() {toggleBLC()});
+$("#hideblcbutton").on('change', function () {
+  toggleBLC()
+});
+$("#acctypebutton").on('change', function () {
+  toggleAccType()
+});
+$("#creationbutton").on('change', function () {
+  toggleCreation()
+});
+$("#blockedbutton").on('change', function () {
+  toggleBlocked()
+});
 
-  function toggleBLC() {
-    var HideBLC = hideblcbutton.checked;
-    
-    chrome.storage.local.set({"HideBLC": HideBLC}, function() {
-        console.log("Saved");
-    });
+function toggleBLC() {
+  var HideBLC = hideblcbutton.checked;
 
-    console.log(HideBLC);
-  }
+  chrome.storage.local.set({
+    "HideBLC": HideBLC
+  }, function () {
+    console.log("Saved");
+  });
 
-  function restore_options() {
-    chrome.storage.local.get(['HideBLC'], function(result) {
-      hideblcbutton.checked = result.HideBLC;
-      console.log("Options loaded: " + result.HideBLC);
-    });
-  }
+  console.log(HideBLC);
+}
 
-  document.addEventListener('DOMContentLoaded', restore_options);
+function toggleAccType() {
+  var AccType = acctypebutton.checked;
+
+  chrome.storage.local.set({
+    "AccType": AccType
+  }, function () {
+    console.log("Saved");
+  });
+
+  console.log(AccType);
+}
+
+function toggleCreation() {
+  var createdAt = creationbutton.checked;
+
+  chrome.storage.local.set({
+    "createdAt": createdAt
+  }, function () {
+    console.log("Saved");
+  });
+
+  console.log(createdAt);
+}
+
+function toggleBlocked() {
+  var blockedNames = blockedbutton.checked;
+
+  chrome.storage.local.set({
+    "blockedNames": blockedNames
+  }, function () {
+    console.log("Saved");
+  });
+
+  console.log(blockedNames);
+}
+
+function restore_options() {
+  chrome.storage.local.get(function (result) {
+    hideblcbutton.checked = result.HideBLC;
+    console.log("Options loaded: " + result.HideBLC);
+    acctypebutton.checked = result.AccType;
+    console.log("Options loaded: " + result.AccType);
+    creationbutton.checked = result.createdAt;
+    console.log("Options loaded: " + result.createdAt);
+    blockedbutton.checked = result.blockedNames;
+    console.log("Options loaded: " + result.blockedNames);
+  });
+}
+
+document.addEventListener('DOMContentLoaded', restore_options);
