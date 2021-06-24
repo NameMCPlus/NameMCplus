0f3a1ea16a5df871b81b2c347afe2403aa0af1ee
Faav <52789876+withdrew@users.noreply.github.com>
2021-06-24 10:50:02 -0400

    Create removeAds.js

---
+++ js/removeAds.js
@@ -0,0 +1,7 @@
+try {
+document.body.style = "padding-top: 0";
+} catch {}
+
+try {
+$("#nn_bfa_wrapper,#nn_skinl,#nn_skinr,#nn_mpu1,#nn_mobile_mpu1,#nn_mobile_mpu2,#mys-wrapper,#nn_mobile_lb1_wrap,#sticky_wrapper").remove();
+} catch {}
