1503b83261e2fae91ce1c676e44436934953a7cc
Faav <52789876+withdrew@users.noreply.github.com>
2021-06-23 22:37:44 -0400

    fix homara- fr

--- js/profile.js
+++ js/profile.js
@@ -23,73 +23,77 @@ function formatCreation(date) {
 setStatus(username);
 
 function setStatus(username) {
-  fetch(`https://api.ashcon.app/mojang/v2/user/${username}`)
+  fetch(`https://api.gapple.pw/cors/username/${username}`)
     .then(response => response.json())
-    .then(ashcon => {
-      fetch(`https://api.gapple.pw/status/${username}`)
+    .then(toUUID => {
+      var uuid = toUUID.id;
+      fetch(`https://api.ashcon.app/mojang/v2/user/${uuid}`)
         .then(response => response.json())
-        .then(gapple => {
-          chrome.storage.local.get(function (result) {
-            var type = gapple.status;
-            var uuid = gapple.uuid
-            var createdAt = ashcon.created_at;
-            var accountType;
-            var tooltip;
+        .then(ashcon => {
+          fetch(`https://api.gapple.pw/status/${username}`)
+            .then(response => response.json())
+            .then(gapple => {
+              chrome.storage.local.get(function (result) {
+                var type = gapple.status;
+                var createdAt = ashcon.created_at;
+                var accountType;
+                var tooltip;
 
-            switch (type) {
-              case "msa":
-                accountType = "Microsoft";
-                tooltip = "Microsoft Account";
-                break;
-              case "migrated_msa":
-                accountType = "Microsoft";
-                tooltip = "Migrated from Mojang";
-                break;
-              case "new_msa":
-                accountType = 'Microsoft';
-                tooltip = "Newly Created";
-                break;
-              case "mojang":
-                accountType = "Mojang";
-                tooltip = "Mojang Account";
-                break;
-              case "normal":
-                accountType = "Normal (Mojang OR Microsoft)";
-                tooltip = "Mojang or Microsoft Account";
-                break;
-              case "legacy":
-                accountType = "Legacy";
-                tooltip = "Unmigrated (2009-Late 2012)";
-                break;
-            }
+                switch (type) {
+                  case "msa":
+                    accountType = "Microsoft";
+                    tooltip = "Microsoft Account";
+                    break;
+                  case "migrated_msa":
+                    accountType = "Microsoft";
+                    tooltip = "Migrated from Mojang";
+                    break;
+                  case "new_msa":
+                    accountType = 'Microsoft';
+                    tooltip = "Newly Created";
+                    break;
+                  case "mojang":
+                    accountType = "Mojang";
+                    tooltip = "Mojang Account";
+                    break;
+                  case "normal":
+                    accountType = "Normal (Mojang OR Microsoft)";
+                    tooltip = "Mojang or Microsoft Account";
+                    break;
+                  case "legacy":
+                    accountType = "Legacy";
+                    tooltip = "Unmigrated (2009 - Late 2012)";
+                    break;
+                }
 
-            if (uuid == "cc7ba16bb2c540fca18eb87b6949270e") { // homara- breaks the API lol)
-              accountType = "Legacy";
-              tooltip = "Unmigrated (2009-Late 2012)";
-            }
-            var createdAtBtn = result.createdAt;
-            var AccTypeBtn = result.AccType;
-            if (AccTypeBtn == true && createdAtBtn == true || AccTypeBtn == undefined && createdAtBtn == undefined) {
-              if (createdAt) {
-                template.innerHTML = `<div class="row no-gutters"><div class="col order-lg-1 col-lg-4"><strong>Account Type</strong></div><div class="col-auto order-lg-3 col-lg-auto text-nowrap text-right" data-toggle="tooltip" title="${tooltip}">${accountType}</div></div><div class="row no-gutters"><div class="col order-lg-1 col-lg-4"><strong>Created At</strong></div><div class="col-auto order-lg-3 col-lg-auto text-nowrap text-right" data-toggle="tooltip" data-html="true" title="<b>Creation dates are inaccurate for a lot of accounts due to a breaking change on Mojang's end. We are currently fetching dates from Ashcon's API. Please yell at Mojang (WEB-3367) in order for accurate creation dates to return.</b>">${formatCreation(createdAt)}</div></div>`;
-              } else {
-                template.innerHTML = `<div class="row no-gutters"><div class="col order-lg-1 col-lg-4"><strong>Account Type</strong></div><div class="col-auto order-lg-3 col-lg-auto text-nowrap text-right" data-toggle="tooltip" title="${tooltip}">${accountType}</div></div>`;
-              }
-            } else if (AccTypeBtn == true && createdAtBtn == false || AccTypeBtn == undefined && createdAtBtn == false) {
-              template.innerHTML = `<div class="row no-gutters"><div class="col order-lg-1 col-lg-4"><strong>Account Type</strong></div><div class="col-auto order-lg-3 col-lg-auto text-nowrap text-right" data-toggle="tooltip" title="${tooltip}">${accountType}</div></div>`;
-            } else if (AccTypeBtn == false && createdAtBtn == true || AccTypeBtn == false && createdAtBtn == undefined) {
-              if (createdAt) {
-                template.innerHTML = `<div class="row no-gutters"><div class="col order-lg-1 col-lg-4"><strong>Created At</strong></div><div class="col-auto order-lg-3 col-lg-auto text-nowrap text-right" data-toggle="tooltip" data-html="true" title="<b>Creation dates are inaccurate for a lot of accounts due to a breaking change on Mojang's end. We are currently fetching dates from Ashcon's API. Please yell at Mojang (WEB-3367) in order for accurate creation dates to return.</b>">${formatCreation(createdAt)}</div></div>`;
-              } else {
-                template.innerHTML = ``;
-              }
-            } else {
-              template.innerHTML = ``;
-            }
-            var viewsElement = document.querySelectorAll(".col-lg-4")[3].parentElement;
-            var accountTypeElement = template.content;
-            viewsElement.parentNode.insertBefore(accountTypeElement, viewsElement.nextSibling);
-          });
+                if (uuid == "cc7ba16bb2c540fca18eb87b6949270e") { // homara- breaks the API lol)
+                  accountType = "Legacy";
+                  tooltip = "Unmigrated (2009 - Late 2012)";
+                }
+                var createdAtBtn = result.createdAt;
+                var AccTypeBtn = result.AccType;
+                if (AccTypeBtn == true && createdAtBtn == true || AccTypeBtn == undefined && createdAtBtn == undefined) {
+                  if (createdAt) {
+                    template.innerHTML = `<div class="row no-gutters"><div class="col order-lg-1 col-lg-4"><strong>Account Type</strong></div><div class="col-auto order-lg-3 col-lg-auto text-nowrap text-right" data-toggle="tooltip" title="${tooltip}">${accountType}</div></div><div class="row no-gutters"><div class="col order-lg-1 col-lg-4"><strong>Created At</strong></div><div class="col-auto order-lg-3 col-lg-auto text-nowrap text-right" data-toggle="tooltip" data-html="true" title="<b>Creation dates are inaccurate for a lot of accounts due to a breaking change on Mojang's end. We are currently fetching dates from Ashcon's API. Please yell at Mojang (WEB-3367) in order for accurate creation dates to return.</b>">${formatCreation(createdAt)}</div></div>`;
+                  } else {
+                    template.innerHTML = `<div class="row no-gutters"><div class="col order-lg-1 col-lg-4"><strong>Account Type</strong></div><div class="col-auto order-lg-3 col-lg-auto text-nowrap text-right" data-toggle="tooltip" title="${tooltip}">${accountType}</div></div>`;
+                  }
+                } else if (AccTypeBtn == true && createdAtBtn == false || AccTypeBtn == undefined && createdAtBtn == false) {
+                  template.innerHTML = `<div class="row no-gutters"><div class="col order-lg-1 col-lg-4"><strong>Account Type</strong></div><div class="col-auto order-lg-3 col-lg-auto text-nowrap text-right" data-toggle="tooltip" title="${tooltip}">${accountType}</div></div>`;
+                } else if (AccTypeBtn == false && createdAtBtn == true || AccTypeBtn == false && createdAtBtn == undefined) {
+                  if (createdAt) {
+                    template.innerHTML = `<div class="row no-gutters"><div class="col order-lg-1 col-lg-4"><strong>Created At</strong></div><div class="col-auto order-lg-3 col-lg-auto text-nowrap text-right" data-toggle="tooltip" data-html="true" title="<b>Creation dates are inaccurate for a lot of accounts due to a breaking change on Mojang's end. We are currently fetching dates from Ashcon's API. Please yell at Mojang (WEB-3367) in order for accurate creation dates to return.</b>">${formatCreation(createdAt)}</div></div>`;
+                  } else {
+                    template.innerHTML = ``;
+                  }
+                } else {
+                  template.innerHTML = ``;
+                }
+                var viewsElement = document.querySelectorAll(".col-lg-4")[3].parentElement;
+                var accountTypeElement = template.content;
+                viewsElement.parentNode.insertBefore(accountTypeElement, viewsElement.nextSibling);
+              });
+            });
         });
     });
 }
