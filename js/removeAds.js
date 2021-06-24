1ac08f229f0ae94e3da6bd85a1532623a255ac50
Faav <52789876+withdrew@users.noreply.github.com>
2021-06-24 19:09:35 -0400

    Update removeAds.js

--- js/removeAds.js
+++ js/removeAds.js
@@ -3,5 +3,5 @@ document.body.style = "padding-top: 0";
 } catch {}
 
 try {
-$("#nn_bfa_wrapper,#nn_skinl,#nn_skinr,#nn_mpu1,#nn_mobile_mpu1,#nn_mobile_mpu2,#mys-wrapper,#nn_mobile_lb1_wrap,#sticky_wrapper").remove();
+$("#nn_bfa_wrapper,#nn_skinl,#nn_skinr,#nn_mpu1,#nn_mobile_mpu1,#nn_mobile_mpu2,#mys-wrapper,#nn_mobile_lb1_wrap,#sticky_wrapper,#nn_mobile_lb1,#nn_1by1").remove();
 } catch {}
