6dd7596f107cd59cbadb7dacc82b4c15935a4c69
Faav <52789876+withdrew@users.noreply.github.com>
2021-06-26 10:46:57 -0400

    Update fixDropdown.js

--- js/fixDropdown.js
+++ js/fixDropdown.js
@@ -2,3 +2,8 @@ $('[data-toggle=dropdown]').dropdown({
   selector: '[data-toggle=dropdown]',
   display: "static"
 });
+
+document.querySelectorAll('[data-toggle=dropdown]').forEach(
+  function (element) {
+    element.classList.add("pointer-all");
+  });
