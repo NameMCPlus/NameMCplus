6a047da821741bea4ab3c56a0f2aad0e65a9fbb4
Faav <52789876+withdrew@users.noreply.github.com>
2021-06-29 21:36:49 -0400

    Update nameSearch.js

--- js/nameSearch.js
+++ js/nameSearch.js
@@ -102,7 +102,9 @@ function betterNameMC(username) {
           } else {
             main.innerHTML = `<div class="mb-3"><div id="status-bar" class="card ${statusClass} ${statusColor}"><div class="card-body px-0"><div class="row align-items-center"><div class="col-md-7"><h1 class="my-1 px-3 text-center text-nowrap text-ellipsis" translate="no"><samp>${username}</samp></h1><hr class="mt-2 mb-1 d-md-none"></div><div class="col-md-5 text-center my-1"><div class="row no-gutters align-items-center"><div class="col-sm-6 my-1"><div> <strong>Status</strong></div><div> ${namemcStatus}</div></div><div class="col-sm-6 my-1"><div> <strong>Searches</strong></div><div class="tabular"> ${searches}</div></div></div></div></div></div></div></div><hr class="mt-0"><div class="row"><div class="col-md-6 col-lg-5 order-md-2"><p class="text-muted">${skinCount}</p>${Skin}</div><div class="col-md-6 col-lg-7 order-md-1"><p class="text-muted">${results}</p>${profiles}<p class="text-muted">${serverCount}</p>${servers}</div></div>`;
           }
+          try {
           drawSkin2D();
+          } catch {}
         } else {
           if (blockedNameBtn !== false) {
             if (status == "blocked" || status == "blocked_or_dropping" && namemcStatus == "Available") {
