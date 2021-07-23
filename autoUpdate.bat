d9a07306b0cc62789d4d4e1b608f5f9ac566c954
M6 <56744133+M6yo@users.noreply.github.com>
2021-07-22 21:29:54 -0400

    Update autoUpdate.bat

--- autoUpdate.bat
+++ autoUpdate.bat
@@ -1,7 +1,7 @@
 @echo off
 cd /d %~dp0
 git init
-git pull
+git pull https://github.com/M6yo/NameMCplus.git main
 echo Now go to your browser's extension settings and press the refresh icon beside NameMC+
 echo Once you've refreshed the extension,
 pause
