8095ac8f0d28da12ae0453055539f6a85a496e40
Orius <32719997+ItsOrius@users.noreply.github.com>
2021-08-30 15:15:17 -0400

    Reimplemented custom skin viewer

--- js/contentScript.js
+++ js/contentScript.js
@@ -1,5 +1,8 @@
+const skinViewer = null;
+const skinViewerWalk = null;
 var username = document.querySelector("[name='profile:username']").content;
 var profileUuid = document.querySelector("body > main > div > div.col-md.order-md-2 > div:nth-child(1) > div.card-body.py-1 > div:nth-child(2) > div.col-12.order-lg-2.col-lg > samp").innerText
+const capeDB = {}
 
 
 
@@ -27,6 +30,8 @@ const capeJsonURL = chrome.runtime.getURL('../json/customCapes.json');
 fetch(capeJsonURL)
     .then(response => response.json())
     .then(json => {
+        createSkinViewer();
+        createSkinEvents();
         const capes = {
             names: [],
             descs: [],
@@ -153,38 +158,178 @@ function createCapeCard(capes, callback = capeCard => {}, {title, redirect, show
 /* Cape canvas creator */
 function createCape(src, parentElement, name = "", description = "", redirect = "") {
     let capeCanvas = document.createElement("canvas");
-        capeCanvas.className = "cape-2d align-top skin-button skin-button-selected";
-        capeCanvas.setAttribute("data-cape-hash", `${name.replace(" ", "-").toLowerCase()}-cape`);
-        capeCanvas.width = 40;
-        capeCanvas.height = 64;
-        capeImage = new Image();
-        capeImage.src = src;
+    capeCanvas.className = "cape-2d align-top skin-button skin-button-selected";
+    let capeDataHash = `custom-${name.replace(" ", "-").toLowerCase()}`;
+    capeCanvas.setAttribute("data-cape-hash", capeDataHash);
+    capeDB[capeDataHash] = src;
+    capeCanvas.width = 40;
+    capeCanvas.height = 64;
+    capeImage = new Image();
+    capeImage.src = src;
 
-        capeImage.onload = () => {
-            const ctx = capeCanvas.getContext('2d');
-            ctx.mozImageSmoothingEnabled = false;
-            ctx.webkitImageSmoothingEnabled = false;
-            ctx.msImageSmoothingEnabled = false;
-            ctx.imageSmoothingEnabled = false;
-            if (capeImage.src != src) capeImage.src = src;
-            const localCapeScale = capeScale(capeImage.height)
-            ctx.drawImage(capeImage, localCapeScale, localCapeScale, 10 * localCapeScale, 16 * localCapeScale, 0, 0, capeCanvas.width, capeCanvas.height)
-        }
+    capeImage.onload = () => {
+        const ctx = capeCanvas.getContext('2d');
+        ctx.mozImageSmoothingEnabled = false;
+        ctx.webkitImageSmoothingEnabled = false;
+        ctx.msImageSmoothingEnabled = false;
+        ctx.imageSmoothingEnabled = false;
+        if (capeImage.src != src) capeImage.src = src;
+        const localCapeScale = capeScale(capeImage.height)
+        ctx.drawImage(capeImage, localCapeScale, localCapeScale, 10 * localCapeScale, 16 * localCapeScale, 0, 0, capeCanvas.width, capeCanvas.height)
+        createCapeEvents();
+    }
 
-        // Puts the image in a href
-        let featureImageHref = document.createElement("a");
-        featureImageHref.href = redirect ? redirect : src;
-        featureImageHref.target = "_blank";
-        featureImageHref.setAttribute("data-toggle", "tooltip"),
-        featureImageHref.setAttribute("data-html", "true")
-        if (typeof name != 'undefined') {
-            featureImageHref.setAttribute("title", `
-                <b>${name}</b>${description ? `<br>${description}` : ""}
-            `)
-        }
-        featureImageHref.appendChild(capeCanvas);
-        parentElement.appendChild(featureImageHref);
+    // Puts the image in a href
+    let featureImageHref = document.createElement("a");
+    featureImageHref.href = redirect ? redirect : src;
+    featureImageHref.target = "_blank";
+    featureImageHref.setAttribute("data-toggle", "tooltip"),
+    featureImageHref.setAttribute("data-html", "true")
+    if (typeof name != 'undefined') {
+        featureImageHref.setAttribute("title", `
+            <b>${name}</b>${description ? `<br>${description}` : ""}
+        `)
+    }
+    featureImageHref.appendChild(capeCanvas);
+    parentElement.appendChild(featureImageHref);
 }
+
+
+
+
+
+/* Creates cape events for the custom viewer */
+function createCapeEvents() {
+    let capeChildren = document.getElementsByClassName("cape-2d")
+    console.log(`Found ${capeChildren.length} capes`)
+    for (var i = 0; i < capeChildren.length; i++) {
+        capeChildren[i].addEventListener('mouseover', (event) => {
+            for (var i = 0; i < capeChildren.length; i++) {
+                capeChildren[i].classList.remove("skin-button-selected");
+            }
+            event.target.classList.add("skin-button-selected");
+            let capeHash = event.target.getAttribute("data-cape-hash")
+
+            if(capeHash != undefined && !capeHash.startsWith("custom-")) {
+                let capeUrl = "https://texture.namemc.com/" + capeHash.substring(0, 2) + "/" + capeHash.substring(2, 4) + "/" + capeHash + ".png";
+                this.skinViewer.loadCape(capeUrl)
+                console.log("capeEvent: Mojang/Optifine")
+            } else if (capeHash.startsWith("custom-")) {
+                this.skinViewer.loadCape(capeDB[capeHash])
+                console.log("capeEvent: Custom")
+            }
+        })
+    }
+}
+
+
+
+
+
+/* Creates a custom skin and cape viewer */
+function createSkinViewer() {
+    // Skin
+    let featureDiv = document.createElement("div");
+    featureDiv.id = "minecraftcapes-skin";
+    featureDiv.className = "card mb-3";
+
+    // Add a button for animation
+    let featureAnimateButton = document.createElement("button");
+    featureAnimateButton.className = "btn btn-secondary play-pause-btn position-absolute top-0 left-0 m-2 p-0";
+    featureAnimateButton.style.cssText = "width:32px;height:32px;z-index:1;";
+    featureAnimateButton.addEventListener('click', (event) => {
+        this.skinViewerWalk.paused = !this.skinViewerWalk.paused;
+    })
+    let featureButtonIcon = document.createElement("i")
+    featureButtonIcon.className = "fas fa-play";
+    featureAnimateButton.appendChild(featureButtonIcon);
+    featureDiv.appendChild(featureAnimateButton);
+
+    // Add a button for Elytra
+    let featureElytraButton = document.createElement("button");
+    featureElytraButton.innerHTML = "Show Elytra"
+    featureElytraButton.className = "btn btn-secondary play-pause-btn position-absolute top-0 right-0 m-2 p-0";
+    featureElytraButton.style.cssText  = "height:32px;padding:0px 10px !important;z-index:1;";
+    featureElytraButton.addEventListener('click', (event) => {
+        if(this.skinViewer.playerObject.backEquipment == "cape") {
+            featureElytraButton.innerHTML = "Show Cape"
+            this.skinViewer.loadCape(this.skinViewer.capeImage, { backEquipment: 'elytra' })
+        } else {
+            featureElytraButton.innerHTML = "Show Elytra"
+            this.skinViewer.loadCape(this.skinViewer.capeImage, { backEquipment: 'cape' })
+        }
+    })
+    featureDiv.appendChild(featureElytraButton);
+
+    // Add the body
+    let featureBody = document.createElement("div");
+    featureBody.className = "card-body text-center checkered";
+
+    featureDiv.appendChild(featureBody);
+
+    // Add the canvas
+    let featureCanvas = document.createElement("canvas");
+    featureCanvas.id = "skin_container"
+    featureBody.appendChild(featureCanvas);
+
+    //Get skin
+    let skinHash = $(".skin-3d").attr("data-skin-hash")
+    let skinUrl = textureURL(skinHash);
+
+    //Insert the div
+    let profileLeft = document.querySelectorAll(".order-md-1 > .card.mb-3")[1]
+    profileLeft.parentElement.insertBefore(featureDiv, profileLeft);
+
+    this.skinViewer = new skinview3d.FXAASkinViewer({
+        canvas: document.getElementById("skin_container"),
+        width: 270,
+        height: 330,
+        skin: skinUrl,
+        cape: this.finalCape,
+        ears: this.finalEars
+    });
+
+    this.skinViewer.loadCape("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEAAAAAgBAMAAABQs2O3AAAAKlBMVEUAAABOTk6NjY2Hh4d7e3tzc3NsbGxZWVlKSkpVVVVoaGiEhIR/f39jY2OSVXT6AAAAAXRSTlMAQObYZgAAAKdJREFUOMtjQAOMgsbGxgz4gCADISDYKCiIX0GHoKAAPgWMQAWClClobBQsx69AYnp5Ah4FnB2SM2vxKphZXj5rAR4F7NOnl6cFYJU6AKHm3kpLC8anYFXaslRnrAoMYAqyQp3xmbA01MUlGqsCBQgV4uri4oRPAatLaIgRVgUboApCXHx24zOBx8ZYSQmfAgYj603YFQTAFChpG+NVwGwEtGIUUBsAADaTIwwcJYk6AAAAAElFTkSuQmCC");
+
+    let control = skinview3d.createOrbitControls(this.skinViewer);
+    control.enableRotate = true;
+    control.enableZoom = false;
+    control.enablePan = false;
+
+    this.skinViewerWalk = this.skinViewer.animations.add(skinview3d.WalkingAnimation);
+    this.skinViewerWalk.paused = true;
+
+    this.skinViewer.camera.position.set(0, 10, 50 );
+    control.update();
+
+    this.skinViewer.playerObject.rotation.y = 6.75
+
+    //Set style
+    document.getElementById("skin_container").style.filter = "drop-shadow(-9px 4px 9px rgba(0,0,0,0.4))"
+    document.getElementById("skin_container").style.outline = "none"
+
+    document.querySelectorAll(".order-md-1 > .card.mb-3")[0].remove();
+}
+
+
+
+
+
+/* Creates skin events for the custom viewer */
+function createSkinEvents() {
+    let skinChildren = document.querySelectorAll("div a .skin-button");
+    for (var i = 0; i < skinChildren.length; i++) {
+      skinChildren[i].addEventListener('mouseover', (event) => {
+        if (event.target != undefined) {
+          if (event.target.getAttribute("data-skin-hash")) {
+            let skinHash = event.target.getAttribute("data-skin-hash")
+            let skinUrl = "https://texture.namemc.com/" + skinHash.substring(0, 2) + "/" + skinHash.substring(2, 4) + "/" + skinHash + ".png";
+            this.skinViewer.loadSkin(skinUrl)
+          }
+        }
+      })
+    }
+  }
 
 
 
@@ -200,4 +345,13 @@ function capeScale(height) {
       return height / 32;
     }
     return Math.max(1, Math.floor(height / 22));
+}
+
+
+
+
+
+/* Returns a NameMC texture URL */
+function textureURL(hash) {
+    return 'https://texture.namemc.com/' + hash[0] + hash[1] + '/' + hash[2] + hash[3] + '/' + hash + '.png';
 }
