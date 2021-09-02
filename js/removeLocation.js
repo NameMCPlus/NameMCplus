2fc3955acf3f7a15c967d7aa3903d26526b8ecf9
M6 <56744133+M6yo@users.noreply.github.com>
2021-09-02 01:02:40 -0400

    Optimizations to the RemoveLocation script

--- js/removeLocation.js
+++ js/removeLocation.js
@@ -1,9 +1,39 @@
 
 chrome.storage.local.get(function (result) {
     if (result.HideLocation == true) {
-      document.querySelector("body > main > div.row > div.col-12.col-md-6.col-lg-5 > div:nth-child(2) > img").remove();
+
+        const DEL_SELECTOR = 'body > main > div.row > div.col-12.col-md-6.col-lg-5 > div:nth-child(2) > img';
+            const mo = new MutationObserver(onMutation);
+            // in case the content script was injected after the page is partially loaded
+            onMutation([{addedNodes: [document.documentElement]}]);
+            observe();
+            function onMutation(mutations) {
+            const toRemove = [];
+            for (const {addedNodes} of mutations) {
+                for (const n of addedNodes) {
+                if (n.tagName) {
+                    if (n.matches(DEL_SELECTOR)) {
+                    toRemove.push(n);
+                    } else if (n.firstElementChild && n.querySelector(DEL_SELECTOR)) {
+                    toRemove.push(...n.querySelectorAll(DEL_SELECTOR));
+                    }
+                }
+                }
+            }
+            if (toRemove.length) {
+                mo.disconnect();
+                for (const el of toRemove) el.remove();
+                observe();
+            }
+            }
+            function observe() {
+            mo.observe(document, {
+                subtree: true,
+                childList: true,
+            });
+            }
+
       document.querySelector("body > main > div.row > div.col-12.col-md-6.col-lg-5 > div:nth-child(2) > div.card-body.py-1 > div:nth-child(4) > div.col-auto.text-nowrap > strong").innerHTML = "Ping";
-      //document.querySelector(".badlion-card").remove();
       a = document.querySelector("body > main > div.row > div.col-12.col-md-6.col-lg-5 > div:nth-child(2) > div.card-body.py-1 > div:nth-child(4) > div.col.text-nowrap.text-right").innerHTML;
       b = a.indexOf("(");
       c = a.slice(b)
