cd47700f538f729eca3652ffa6b972a3bceeb6fe
Orius <32719997+ItsOrius@users.noreply.github.com>
2021-09-02 18:51:41 -0400

    Removed useless file (badlion card)

--- js/skin.js
+++
@@ -1,8 +0,0 @@
-chrome.storage.local.get(function (result) {
-  console.log(result.HideBLC);
-  if (result.HideBLC == true) {
-    try {
-      document.querySelector(".badlion").remove();
-    } catch {}
-  }
-});
