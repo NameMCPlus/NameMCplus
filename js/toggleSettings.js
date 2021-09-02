43e4ee4573491f5deeda18005075cd7716c9637d
Orius <32719997+ItsOrius@users.noreply.github.com>
2021-09-02 18:33:56 -0400

    REMADE THE ENTIRE CODEBASE AAAAAH
    I still gotta update the other scripts to match the new name style :P

--- js/toggleSettings.js
+++ js/toggleSettings.js
@@ -1,200 +1,46 @@
-$("#acctypebutton").on('change', function () {
-  toggleAccType()
-});
-$("#hideseparationbutton").on('change', function () {
-  toggleSeparation()
-});
-$("#creationbutton").on('change', function () {
-  toggleCreation()
-});
-$("#blockedbutton").on('change', function () {
-  toggleBlocked()
-});
-$("#searchbutton").on('change', function () {
-  toggleBTSearch()
-});
-$("#othercapesbutton").on('change', function () {
-  toggleOtherCapes()
+class Setting {
+  constructor(name, button, defaultOption = false) {
+    this.name = name;
+    this.button = button;
+    this.defaultOption = defaultOption;
+  }
+}
+
+const options = [
+  new Setting("otherCapes", document.getElementById("othercapesbutton"), true),
+  new Setting("accountTypes", document.getElementById("acctypebutton"), true),
+  new Setting("creationDates", document.getElementById("creationbutton"), true),
+  new Setting("blockedNames", document.getElementById("blockedbutton"), true),
+  new Setting("oldLayout", document.getElementById("searchbutton"), false),
+  new Setting("hideLocation", document.getElementById("hidelocationbutton"), false),
+  new Setting("hideSeparation", document.getElementById("hideseparationbutton"), false),
+  new Setting("mcusername", document.getElementById("mcusernamebutton"), false)
+]
+
+options.forEach(option => {
+  option.button.onclick = () => {
+    toggleSetting(option.name, option.button.checked)
+  }
 })
-$("#hidelocationbutton").on('change', function () {
-  toggleLocation()
-})
-$("#mcusernamebutton").on('change', function () {
-  toggleMCusername()
-})
 
-function toggleLocation() {
-  var HideLocation = hidelocationbutton.checked;
-
-  chrome.storage.local.set({
-    "HideLocation": HideLocation
-  }, function () {
-    console.log("Saved");
-  });
-
-  console.log(HideLocation);
-}
-
-function toggleSeparation() {
-  var HideSeparation = hideseparationbutton.checked;
-
-  chrome.storage.local.set({
-    "HideSeparation": HideSeparation
-  }, function () {
-    console.log("Saved");
-  });
-
-  console.log(HideSeparation);
-}
-
-function toggleAccType() {
-  var AccType = acctypebutton.checked;
-
-  chrome.storage.local.set({
-    "AccType": AccType
-  }, function () {
-    console.log("Saved");
-  });
-
-  console.log(AccType);
-}
-
-function toggleCreation() {
-  var createdAt = creationbutton.checked;
-
-  chrome.storage.local.set({
-    "createdAt": createdAt
-  }, function () {
-    console.log("Saved");
-  });
-
-  console.log(createdAt);
-}
-
-function toggleBlocked() {
-  var blockedNames = blockedbutton.checked;
-
-  chrome.storage.local.set({
-    "blockedNames": blockedNames
-  }, function () {
-    console.log("Saved");
-  });
-
-  console.log(blockedNames);
-}
-
-function toggleBTSearch() {
-  var BTSearch = searchbutton.checked;
-
-  chrome.storage.local.set({
-    "BTSearch": BTSearch
-  }, function () {
-    console.log("Saved");
-  });
-
-  console.log(BTSearch);
-}
-
-function toggleOtherCapes() {
-  var otherCapes = othercapesbutton.checked;
-
-  chrome.storage.local.set({
-    "otherCapes": otherCapes
-  }, function () {
-    console.log("Saved");
-  });
-
-  console.log(otherCapes);
-}
-
-function toggleMCusername() {
-  var mcusername = mcusernamebutton.checked;
-
-  chrome.storage.local.set({
-    "mcusername": mcusername
-  }, function () {
-    console.log("Saved");
-  });
-
-  console.log(mcusername);
+function toggleSetting(key, value) {
+  const obj = {};
+  obj[key] = value;
+  chrome.storage.local.set(obj, console.log(`Successfully set "${key}" to "${value}"`))
 }
 
 function restore_options() {
   chrome.storage.local.get(function (result) {
-
-    var AccType = result.AccType;
-    var createdAt = result.createdAt;
-    var blockedNames = result.blockedNames;
-    var BTSearch = result.BTSearch;
-    var HideLocation = result.HideLocation;
-    var HideSeparation = result.HideSeparation;
-    var otherCapes = result.otherCapes;
-    var mcusername = result.mcusername;
-
-    if (AccType == undefined) {
-      acctypebutton.checked = true;
-      AccType = true;
-      toggleAccType()
-    }
-    if (createdAt == undefined) {
-      creationbutton.checked = true;
-      createdAt = true;
-      toggleCreation()
-    }
-    if (blockedNames == undefined) {
-      blockedbutton.checked = true;
-      blockedNames = true;
-      toggleBlocked()
-    }
-    if (BTSearch == undefined) {
-      searchbutton.checked = true;
-      BTSearch = true;
-      toggleBTSearch()
-    }
-    if (otherCapes == undefined) {
-      othercapesbutton.checked = true;
-      otherCapes = true;
-      toggleOtherCapes();
-    }
-    if (HideLocation == undefined) {
-      hidelocationbutton.checked = false;
-      HideLocation = false;
-      toggleLocation()
-    }
-    if (HideSeparation == undefined) {
-      hideseparationbutton.checked = false;
-      HideSeparation = false;
-      toggleSeparation()
-    }
-    if (mcusername == undefined) {
-      mcusernamebutton.checked = false;
-      mcusername = false;
-      toggleMCusername()
-    }
-
-    acctypebutton.checked = AccType;
-    console.log("Options loaded: " + AccType);
-
-    creationbutton.checked = createdAt;
-    console.log("Options loaded: " + createdAt);
-
-    blockedbutton.checked = blockedNames;
-    console.log("Options loaded: " + blockedNames);
-
-    searchbutton.checked = BTSearch;
-    console.log("Options loaded: " + BTSearch);
-
-    hidelocationbutton.checked = HideLocation;
-    console.log("Options loaded: " + HideLocation);
-
-    hideseparationbutton.checked = HideSeparation;
-    console.log("Options loaded: " + HideSeparation);
-
-    othercapesbutton.checked = otherCapes;
-    console.log("Options loaded: " + otherCapes)
-
-    mcusernamebutton.checked = mcusername;
-    console.log("Options loaded: " + mcusername)
+    options.forEach(option => {
+      if (result[option.name] == undefined) {
+        option.button.checked = option.defaultOption;
+        toggleSetting(option.name, option.defaultOption);
+        console.log(`Option ${option.name} didn't exist`)
+        return;
+      }
+      console.log(`Option ${option.name} is set to ${result[option.name]}`)
+      option.button.checked = result[option.name];
+    })
   });
 }
 
