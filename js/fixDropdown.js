9b62e6039fc6495357613fa23f30c2e272160e7f
M6 <56744133+M6yo@users.noreply.github.com>
2021-09-29 22:51:27 -0400

    Update fixDropdown.js

--- js/fixDropdown.js
+++ js/fixDropdown.js
@@ -7,3 +7,5 @@ document.querySelectorAll('[data-toggle=dropdown]').forEach(
   function (element) {
     element.classList.add("pointer-all");
   });
+
+document.querySelector("#header > div:nth-child(1) > nav > a > span").innerText = "NameMC+";
